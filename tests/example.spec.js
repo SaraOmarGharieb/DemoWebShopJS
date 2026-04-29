9b5d2d38fbce8c4b2b3b577b88aee4a83296c80b
Gharieb <sara.gharieb@capgemini.com>
2026-04-29 17:14:56 +0300

    firstcommit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,14 +2,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.google.com.eg/index.html');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.google.com.eg/index.html');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
